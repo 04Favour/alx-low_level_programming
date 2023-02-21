48b9119c0d3febc94fa5833719462cce00bc3990
Favour Mfon <favourmichael004@gmail.com>
2023-02-21 15:35:09 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * print_alphabet: functions thatbprints in lower case
+ * Return: 0 as always
+ */
+
+void print_alphabet(void);
+{
+	char i ;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
