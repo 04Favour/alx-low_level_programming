015d9467165be609f39bcce7595f9ccbcd59b3b6
Favour Mfon <favourmichael004@gmail.com>
2023-02-22 06:19:56 -0800

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,13 @@
  * print_alphabet: functions thatbprints in lower case
  * Return: 0 as always
  */
-
 void print_alphabet(void);
 {
-	char i ;
+	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
 }
