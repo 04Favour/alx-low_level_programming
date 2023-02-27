335ca6921e23d40d3987b79612021f6171536dd9
Favour Mfon <favourmichael004@gmail.com>
2023-02-27 08:50:22 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c: checks the character
+ * Return: 1 if character is uppercase, else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+
+
+
+}
