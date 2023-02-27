6fe16ecae2118755e22645cec1a8f986b17b5fc3
Favour Mfon <favourmichael004@gmail.com>
2023-02-27 09:14:57 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - it checks digits
+ * @c: the digit checked
+ * Return: Returns 1 if c is a digit,Returns 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
