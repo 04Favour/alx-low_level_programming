82bf3db6ff73391cde21c17279eb3d7795b972ce
Favour Mfon <favourmichael004@gmail.com>
2023-02-27 09:36:36 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of the code
+ * @a: integer to be multiplied
+ * @b: integer to be also multiplied
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
