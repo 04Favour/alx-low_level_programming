f1ce1f2d9d37654813976b355b3dd3c0d4f46043
Favour Mfon <favourmichael004@gmail.com>
2023-02-27 19:15:13 -0800

    modified

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
- * @n: integer pointed at
- *
+ * and updates the value it points to 98.
+ * @n: input
+ * Return: n
  */
 void reset_to_98(int *n)
 {
