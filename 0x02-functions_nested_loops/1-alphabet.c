07d9dd7ef9a362f64369bcbe40129d36192746c9
Favour Mfon <favourmichael004@gmail.com>
2023-06-20 03:17:00 -0700

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase.
+ */
+
+void print_alphabet(void)
+{
+
+	int lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+
+		_putchar(lc);
+	}
+	_putchar('\n');
+}
